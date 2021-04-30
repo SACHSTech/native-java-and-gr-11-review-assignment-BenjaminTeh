f06fb7438a3a9da263809c86976586c330a69fbb
Benjamin Teh <>
2021-04-30 00:09:35 +0000

    Review1 V1

--- src/gr11review/part1/Review1.java
+++ src/gr11review/part1/Review1.java
@@ -1,1 +1,17 @@
 package gr11review.part1;
+import java.io.*;
+
+/** 
+* Program that takes the month, converts it to the number of days from the start of the year to the end of the month before it, and adds the to the day of the month the user entered.
+@author Benjamin Teh
+*/
+public class Review1{
+    public static void main(String[] args) throws IOException{
+      int numMonth;
+      int numDay;
+
+
+
+
+    }
+}
