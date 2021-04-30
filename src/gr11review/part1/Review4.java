37f83122fddffb001db7ee7c3147772aec4d1996
Benjamin Teh <>
2021-04-30 18:39:50 +0000

    Review4 V1

--- src/gr11review/part1/Review4.java
+++ src/gr11review/part1/Review4.java
@@ -1,1 +1,7 @@
 package gr11review.part1;
+import java.io.*;
+
+/**
+*Take the amount of items the user wants to buy. Ask for the prices using a loop that runs the exact amount of times as the number of items the user is buying.
+*Display the subtotal, display the 13% tax, and display the total
+*/
