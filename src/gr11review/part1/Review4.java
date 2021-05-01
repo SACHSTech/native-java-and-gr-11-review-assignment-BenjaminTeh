b0d21ad13974f30c26f09d750280e2d6ae893489
Benjamin Teh <>
2021-05-01 01:58:43 +0000

    Review4 V3

--- src/gr11review/part1/Review4.java
+++ src/gr11review/part1/Review4.java
@@ -1,5 +1,6 @@
 package gr11review.part1;
 import java.io.*;
+import java.text.*;
 
 /**
 *Take the amount of items the user wants to buy. Ask for the prices using a loop that runs the exact amount of times as the number of items the user is buying.
@@ -9,15 +10,19 @@ import java.io.*;
 public class Review4{
   public static void main(String[] args) throws IOException{
     BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+    NumberFormat numberFormat = new DecimalFormat("#,###.00");
     int itemCount;
-    int subTotal;
+    double itemPrice;
+    double subTotal;
     int priceCounter;
 
     System.out.println("How many items do you want to buy?");
     itemCount = Integer.parseInt(keyboard.readLine());
 
-    for(priceCounter = 0; priceCounter < itemCount; priceCounter++){
-      
+    for(priceCounter = 1; priceCounter <= itemCount; priceCounter++){
+      System.out.println("Enter the price for item " + priceCounter + ":");
+      itemPrice = Double.parseDouble(keyboard.readLine());
+      System.out.println(numberFormat.format(itemPrice));
     }
 
   }
